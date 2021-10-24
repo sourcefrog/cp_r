7842586686bd8c4058944e6b8a81e28f3067e5d2
Martin Pool <mbp@sourcefrog.net>
2021-10-24 15:16:14 -0700

    Test compat with anyhow

---
+++ tests/anyhow.rs
@@ -0,0 +1,33 @@
+// Copyright 2021 Martin Pool
+
+//! Test compatibility with Anyhow.
+
+use std::path::Path;
+
+use anyhow::Context;
+use cp_r::{copy_tree, CopyOptions};
+
+#[test]
+fn attach_anyhow_context_to_success() {
+    // This is mostly an assertion that the error type is compatible with that expected by Anyhow.
+    let dest = tempfile::tempdir().unwrap();
+    let stats = copy_tree(&Path::new("src"), &dest.path(), &CopyOptions::new())
+        .context("copy src dir for test")
+        .unwrap();
+    dbg!(&stats);
+}
+
+#[test]
+fn attach_anyhow_context_to_failure() {
+    // This is mostly an assertion that the error type is compatible with that expected by Anyhow.
+    let dest = tempfile::tempdir().unwrap();
+    let err = copy_tree(
+        &Path::new("src"),
+        &dest.path().join("nonexistent"),
+        &CopyOptions::new(),
+    )
+    .context("copy src dir for test")
+    .unwrap_err();
+    dbg!(&err);
+    println!("Display error: {}", err);
+}
