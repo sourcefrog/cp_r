fd979565bbf6e9cd1b8561abe498ea5b6fec5ba2
Martin Pool <mbp@sourcefrog.net>
2024-10-07 08:04:49 -0700

    Expect links are relative on Windows

--- tests/windows.rs
+++ tests/windows.rs
@@ -1,6 +1,8 @@
 #![cfg(windows)]
 
 use std::fs::{create_dir, metadata, read_link, read_to_string, symlink_metadata, write};
+use std::os::windows::fs::{symlink_dir, symlink_file};
+use std::path::Path;
 
 use tempfile::TempDir;
 
@@ -11,14 +13,14 @@ fn copy_file_symlink() {
     let tmp = TempDir::with_prefix("src").unwrap();
     let src = tmp.path();
     write(src.join("target"), b"hello").unwrap();
-    std::os::windows::fs::symlink_file("target", src.join("link")).unwrap();
+    symlink_file("target", src.join("link")).unwrap();
 
     let dest_tmp = TempDir::with_prefix("dest").unwrap();
     let dest = dest_tmp.path();
     CopyOptions::new().copy_tree(src, dest).unwrap();
 
     assert_eq!(read_to_string(dest.join("link")).unwrap(), "hello");
-    assert_eq!(read_link(dest.join("link")).unwrap(), dest.join("target"));
+    assert_eq!(read_link(dest.join("link")).unwrap(), Path::new("target"));
 }
 
 #[test]
@@ -26,7 +28,7 @@ fn copy_dir_symlink() {
     let tmp = TempDir::with_prefix("src").unwrap();
     let src = tmp.path();
     create_dir(src.join("target")).unwrap();
-    std::os::windows::fs::symlink_dir("target", src.join("link")).unwrap();
+    symlink_dir("target", src.join("link")).unwrap();
     println!(
         "source symlink target is {:?}",
         read_link(src.join("link")).unwrap()
@@ -42,5 +44,5 @@ fn copy_dir_symlink() {
 
     assert!(symlink_metadata(dest.join("link")).unwrap().is_symlink());
     assert!(metadata(dest.join("link")).unwrap().is_dir());
-    assert_eq!(read_link(dest.join("link")).unwrap(), dest.join("target"));
+    assert_eq!(read_link(dest.join("link")).unwrap(), Path::new("target"));
 }
