6a1c400f9c31188ce3da533f0436e2a5d4dc46e6
Martin Pool <mbp@sourcefrog.net>
2024-10-06 17:53:26 -0700

    See if Windows links are absolute?

--- tests/windows.rs
+++ tests/windows.rs
@@ -8,12 +8,12 @@ use cp_r::CopyOptions;
 
 #[test]
 fn copy_file_symlink() {
-    let tmp = TempDir::new().unwrap();
+    let tmp = TempDir::with_prefix("src").unwrap();
     let src = tmp.path();
     write(src.join("target"), b"hello").unwrap();
     std::os::windows::fs::symlink_file("target", src.join("link")).unwrap();
 
-    let dest_tmp = TempDir::new().unwrap();
+    let dest_tmp = TempDir::with_prefix("dest").unwrap();
     let dest = dest_tmp.path();
     CopyOptions::new().copy_tree(src, dest).unwrap();
 
@@ -23,14 +23,22 @@ fn copy_file_symlink() {
 
 #[test]
 fn copy_dir_symlink() {
-    let tmp = TempDir::new().unwrap();
+    let tmp = TempDir::with_prefix("src").unwrap();
     let src = tmp.path();
     create_dir(src.join("target")).unwrap();
     std::os::windows::fs::symlink_dir("target", src.join("link")).unwrap();
+    println!(
+        "source symlink target is {:?}",
+        read_link(src.join("link")).unwrap()
+    );
 
-    let dest_tmp = TempDir::new().unwrap();
+    let dest_tmp = TempDir::with_prefix("dest").unwrap();
     let dest = dest_tmp.path();
     CopyOptions::new().copy_tree(&src, &dest).unwrap();
+    println!(
+        "dest symlink target is {:?}",
+        read_link(dest.join("link")).unwrap()
+    );
 
     assert!(symlink_metadata(dest.join("link")).unwrap().is_symlink());
     assert!(metadata(dest.join("link")).unwrap().is_dir());
